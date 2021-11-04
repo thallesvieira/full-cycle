a7bfdc843093774500431f7d71f56b926cc6cc47
Thalles <thalles_jacobs@hotmail.com>
2021-11-04 07:28:28 -0300

    nginx, node and mysql

--- node/index.js
+++ node/index.js
@@ -1,23 +1,49 @@
-const { application } = require('express')
+const { application, response } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
-/*const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-};
+var responseStr = "MySQL Data:";
+const nomes =["Luan","Pedro","Caio", "Mariana", "Lígia", "Rafaela"]
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
-const sql = `INSERT INTO people (name) values ('Thalles')`
-connection.query(sql)
-connection.end()
-*/
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+app.get('/',function(req,res){
+    const config = {
+        host: 'db',
+        user: 'root',
+        password: '123',
+        database: 'nodedb'
+    };
+
+    console.log("conectando...")
+    const i = Math.floor(Math.random() * (5 - 0)) + 0;
+    const nome = nomes[i];
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+    const insertQuery = `INSERT INTO people (name) values ('`+nome+`')`;
+    const selectQuery = 'SELECT * FROM people;';
+    
+    connection.connect();
+    console.log("conectado")
+    
+    connection.query(insertQuery);
+    connection.query(selectQuery, function (err, results, fields) {
+        responseStr = '';
+        
+        results.forEach(function(data) {
+            console.log(data)
+            responseStr += '<p>Nome: ' + data.name + '</p>\n'  
+        });
+
+        if (responseStr.length == 0)  
+            responseStr = 'Nenhuma informação encontrada';
+
+        console.log(responseStr);
+        res.status(200).send('<h1>Full Cycle</h1>\n' + 
+                            '<h3>Tabela People</h3> \n' +
+                            responseStr);
+    });
+
+    connection.end();
+});
 
 app.listen(port, () =>{
     console.log('Rodando na porta ' + port)
